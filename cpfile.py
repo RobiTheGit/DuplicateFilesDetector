164af9ca8599b573d7d52942c93601c91616b479
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-07-23 14:14:45 -0600

    added some comments for the examples

--- cpfile.py
+++ cpfile.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+'''
+Import the RMDup Module
+'''
 import rmdup
+'''
+Import the OS Operating System Functions Module 
+'''
 import os
+'''
+Give the user input for the file (It can be Absolute or Relative)
+'''
 INPUT = input("File to copy: ")
+'''
+Give the user output for the file name for the copy (It can be Absolute or Relative)
+'''
 OUTPUT = input("Name of Copy: ")
+'''
+Run RMDup's File Duplicator With The Input and Output files
+'''
 rmdup.CopyFile(INPUT,OUTPUT)
--- randomscript.py
+++ randomscript.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+'''
+Import the RMDup Module
+'''
 import rmdup
+'''
+Import the OS Operating System Functions Module 
+'''
 import os
+'''
+Run RMDup's Duplicate File Detector in the Current Working Directory
+'''
 rmdup.RMDup(os.getcwd())
